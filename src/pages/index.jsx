ef801b3d023e2aa98572da3abdb8e890da40ddd3
JacobDFrank <frankjacob42@gmail.com>
2018-03-28 16:21:54 -0400

    removed innerds of mailchimp component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import Eboard from './components/Eboard'
+import Mailchimp from './components/Mailchimp'
 import About from './components/About'
 
 const IndexPage = ({data}) => (
   <div>
     <About/>
+    <Mailchimp/>
     <Eboard/>
   </div>
 )
