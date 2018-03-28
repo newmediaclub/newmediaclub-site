479108757cd4f0700eaa9ab4a21cb1c730b66478
JacobDFrank <frankjacob42@gmail.com>
2018-03-28 14:18:31 -0400

    removed mailchimp component for netlify build to work

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import Eboard from './components/Eboard'
-import Mailchimp from './components/Mailchimp'
 import About from './components/About'
 
 const IndexPage = ({data}) => (
   <div>
     <About/>
-    <Mailchimp/>
     <Eboard/>
   </div>
 )
