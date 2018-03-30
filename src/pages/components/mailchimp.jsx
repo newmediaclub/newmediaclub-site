56a1e92fb2fb298e34ea1e45d6df15b06d70408a
JacobDFrank <frankjacob42@gmail.com>
2018-03-30 12:22:33 -0400

    removed messy contents

--- src/pages/components/mailchimp.jsx
+++ src/pages/components/mailchimp.jsx
@@ -1,33 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import SubscribeFrom from "react-mailchimp-subscribe";
-import Adam_head from '../../assets/Adam_head.png';
+
 
 class Mailchimp extends React.Component {
   render() {
     return (<div className="email-container flex flex-justify-center flex-align-items-center" id="mc_embed_signup">
-      <form className="validate flex flex-row" action="https://rit.us14.list-manage.com/subscribe/post?u=8ed84f128f439d4ab46e02db7&amp;id=e367bf5ac6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" noValidate="novalidate">
-        <input className="email-input required email" type="email" name="EMAIL" id="mce-EMAIL" required="" placeholder="Give me the emails"></input>
-        <div id="mce-responses" className="clear">
-          <div className="response" id="mce-error-response" style={{
-              display: 'none'
-            }}></div>
-          <div className="response" id="mce-success-response" style={{
-              display: 'none'
-            }}></div>
-          <div style={{
-              position: 'absolute',
-              left: '-5000px'
-            }} aria-hidden="true">
-            <input type="text" name="b_8ed84f128f439d4ab46e02db7_e367bf5ac6" tabIndex="-1" value=""></input>
-          </div>
-          <div className="email-button-container clear">
-            
-            <input type="submit" value="Let's do this" name="subscribe" id="mc-embedded-subscribe" className="button email-button position-relative"></input>
-          </div>
-        </div>
-      </form>
-    </div>)
+      </div>)
   }
 }
 
