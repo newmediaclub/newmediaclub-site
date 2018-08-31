a1e38e05640d6f35981863401ce1df96ffb651cd
JacobDFrank <frankjacob42@gmail.com>
2018-08-31 11:51:07 -0400

    fixed up error

--- src/pages/components/mailchimp.jsx
+++ src/pages/components/mailchimp.jsx
@@ -5,7 +5,7 @@ import Link from 'gatsby-link'
 class Mailchimp extends React.Component {
   render() {
     return (<div className="email-container flex flex-justify-center flex-align-items-center" id="mc_embed_signup">
-      </div>)
+    </div>)
   }
 }
 
