e4ff4f319d0680ab018fe7437db93152d9daf2d5
Andrew Hurley <hurleymandrew@gmail.com>
2018-09-18 21:26:20 -0400

    added json switching functionality

--- src/pages/components/Eboard.jsx
+++ src/pages/components/Eboard.jsx
@@ -4,7 +4,15 @@ import data from '../../assets/data.json';
 import mentor from '../../assets/mentorData.json';
 var jsonFile = mentor;
 class Eboard extends React.Component {
-
+	constructor(props){
+		super(props);
+		this.state = {
+			selected : "data"
+		}
+		// this.handleClick = this.handleClick.bind(this);
+		this.setData = this.setData.bind(this);
+		this.setMentor = this.setMentor.bind(this);
+	}
 	createPerson = (person) => {
 		return <Person imageURL={person.image} key={person.name} name={person.name} title={person.title} bio={person.bio}/>;
 	};
@@ -13,46 +21,50 @@ class Eboard extends React.Component {
 		return people.map(this.createPerson);
 	};
 
-	// createPerson = (mentor) => {
-	// 	return <Person imageURL={mentor.image} key={mentor.name} name={mentor.name} title={mentor.title} bio={mentor.bio}/>;
-	// };
-	//
-	// createPeople = (mentor) => {
-	// 	return mentor.map(this.createPerson);
-	// };
+	setData(){
 
-	handleClick(){
-		// if (jsonFile == "data") {
-		// 	jsonFile = "mentor";
-		// 	console.log("mentordata");
-		// 	createPerson = (person) => {
-		// 		return <Person imageURL={person.image} key={person.name} name={person.name} title={person.title} bio={person.bio}/>;
-		// 	};
-		// 	createPeople = (people) => {
-		// 		return people.map(this.createPerson);
-		// 	};
-		// }else if (jsonFile == "mentor"){
-		// 	jsonFile = "data";
-		// 	console.log("eboarddata");
-		// }else{
-		// 	jsonFile = "data";
-		// 	console.log("fallbackeboard");
-		// }
+		this.setState({
+			selected : "data"
+		});
+
 	};
+	setMentor(){
+
+		this.setState({
+			selected : "mentor"
+		});
+
+	};
+	// handleClick(){
+	//
+	// 	if (this.state.selected == "data") {
+	// 		this.setState({
+	// 			selected : "mentor"
+	// 		});
+	// 	}else if (this.state.selected == "mentor"){
+	// 		this.setState({
+	// 			selected : "data"
+	// 		});
+	// 	}
+	//
+	// };
 	render() {
+
+	 const currentData = this.state.selected === "mentor" ? mentor : data;
+
 		return (
 			<div className="container">
 				<div className="buttonContainer">
-					<button onClick={this.handleClick} className="buttonPeople">
+					<button onClick={this.setData} className="buttonPeople">
 						Eboard
 					</button>
-					<button onClick={this.handleClick} className="buttonPeople buttonPeople-active" >
+					<button onClick={this.setMentor} className="buttonPeople buttonPeople-active" >
 						Mentors
 					</button>
 				</div>
 				<div className="eboard">
 					<h1 className="eboard-header">Brought to you by</h1>
-					<div className="eboard-people">{this.createPeople(jsonFile.people)}</div>
+					<div className="eboard-people">{this.createPeople(currentData.people)}</div>
 				</div>
 			</div>
 		);
