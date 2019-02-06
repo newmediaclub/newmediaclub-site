eb3ddef50a087f6a4c004b39ade6548b7fca6c0f
Andrew Hurley <hurleymandrew@gmail.com>
2019-02-06 17:21:34 -0500

    created mentoreboard cards and json functionality

--- src/components/Eboard.jsx
+++ src/components/Eboard.jsx
@@ -9,16 +9,16 @@ class Eboard extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-      selected : "data",
-      button_active__eboard: "button_active__eboard",
-      button_active__mentor: "",
+			selected : "data",
+			button_active__eboard: "button_active__eboard",
+			button_active__mentor: "",
 		}
 		// this.handleClick = this.handleClick.bind(this);
 		this.setData = this.setData.bind(this);
 		this.setMentor = this.setMentor.bind(this);
 	}
 	createPerson = (person) => {
-		return <Person imageURL={person.image} key={person.name} name={person.name} title={person.title} bio={person.bio}/>;
+		return <Person imageURL={person.image} key={person.name} name={person.name} title={person.title} bio={person.bio} color={person.color}/>;
 	};
 
 	createPeople = (people) => {
@@ -28,35 +28,35 @@ class Eboard extends React.Component {
 	setData = () => {
 
 		this.setState({
-      selected : "data",
-      button_active__eboard: "button_active__eboard",
-      button_active__mentor: ""
+			selected : "data",
+			button_active__eboard: "button_active__eboard",
+			button_active__mentor: ""
 		});
 
 	};
 	setMentor = () => {
 
 		this.setState({
-      selected : "mentor",
-      button_active__mentor: "button_active__mentor",
-      button_active__eboard: ""
+			selected : "mentor",
+			button_active__mentor: "button_active__mentor",
+			button_active__eboard: ""
 		});
 
 	};
 	render() {
 
-   const currentData = this.state.selected === "mentor" ? mentor : data;
-if (this.state.selected == data) {
+		const currentData = this.state.selected === "mentor" ? mentor : data;
+		if (this.state.selected == data) {
 
-}
+		}
 
 		return (
 			<div className="container">
 				<div className="buttonContainer">
-          <div onClick={this.setData} className={classnames('buttonPeople', this.state.button_active__eboard)}>
+					<div onClick={this.setData} className={classnames('buttonPeople', this.state.button_active__eboard)}>
 						Eboard
 					</div>
-          <div onClick={this.setMentor} className={classnames('buttonPeople', this.state.button_active__mentor)}>
+					<div onClick={this.setMentor} className={classnames('buttonPeople', this.state.button_active__mentor)}>
 						Mentors
 					</div>
 				</div>
--- src/components/Members/Person.jsx
+++ src/components/Members/Person.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 
 let Person = function statelessFunctionComponentClass(props) {
-  let imageURL = '../assets/' + props.imageURL
+	let imageURL = '../assets/' + props.imageURL
+	let myColor = props.color ;
+	console.log(props.color);
+	let cardColor = function(myColor) {
+		return {
+			background: myColor,
+		}
+	}
+	let allTerms = "design visual";
+	let sortTerm = allTerms;
+	console.log(sortTerm);
+	if (props.title.indexOf(sortTerm)>=0) {
+		console.log("yes it does");
+		console.log(props.title);
+	}else{
+		console.log("no it does not");
+	}
+	return (
+//if prop title contains "motion design" then display block else none
+		<div className="personCard">
+			<div className="colorSwatch" style={cardColor(myColor)}></div>
+			<img className="photo" src={__PATH_PREFIX__ + imageURL} />
+			<div className="personInfo">
+				<div className="name">{props.name}</div>
+				<div className="title">{props.title}</div>
+				<div className="description">{props.bio}</div>
+			</div>
+		</div>
 
-  return (<div className="eboard-person grid grid__col grid__col--3-of-6 ">
-
-    <div className="eboard-image grid__col grid__col--m-6-of-6">
-      <img src={__PATH_PREFIX__ + imageURL} />
-    </div>
-
-    <div className="eboard-content grid__col grid__col--m-6-of-6">
-      <p className="eboard-title">{props.title}</p>
-      <h2 className="eboard-name">{props.name}</h2>
-      <p className="eboard-bio">{props.bio}</p>
-    </div>
-
-  </div>)
+	)
 }
 
 export default Person
