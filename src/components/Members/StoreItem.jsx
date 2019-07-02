31c8e6f4e4c85dc9b341cb6e2f954d02d5b46f4f
Andrew Hurley <hurleymandrew@gmail.com>
2019-07-02 10:39:46 -0400

    added store and added jane brennan to the eboard - removed aditi from eboard

---
+++ src/components/Members/StoreItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+let StoreItem = function statelessFunctionComponentClass(props) {
+
+	let imageURL = '../storeAssets/' + props.image;
+
+	return (
+
+		<div className="resourceCardContainer">
+
+				<a href={props.link} target="_blank"><img src={imageURL} className="resourceImage"></img></a>
+
+				<div className="resourceTitle">{props.title}</div>
+		</div>
+	)
+}
+
+export default StoreItem
