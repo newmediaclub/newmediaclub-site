dcff69438e196ae371c7d5880de665fedcd32d90
Andrew Hurley <hurleymandrew@gmail.com>
2019-07-23 15:42:10 -0400

    grid fixed withe magical attribute max-height

--- src/components/Spotlights.jsx
+++ src/components/Spotlights.jsx
@@ -13,7 +13,7 @@ class Spotlights extends React.Component {
 	}
 
 	createEvent = (spotlight) => {
-		return <EventCard date={spotlight.date} title={spotlight.title} description={spotlight.description} link={spotlight.link} image={spotlight.image} buttonText={spotlight.buttonText} account={spotlight.account}/>;
+		return <EventCard date={spotlight.date} key={spotlight.title} title={spotlight.title} description={spotlight.description} link={spotlight.link} image={spotlight.image} buttonText={spotlight.buttonText} account={spotlight.account}/>;
 	};
 
 
